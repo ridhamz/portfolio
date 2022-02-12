b8687d98769df238a3498c4356333972e83c0018
ridha mezrigui <ridhamezrigui07@gmail.com>
2022-02-12 22:15:18 +0100

    Updates

---
+++ precache-manifest.ee145f844ba810b7fae06cf9ffe3e641.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395ed8a1d1bae765966722b207d226eb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "90991ed07a6a8ed1b889",
+    "url": "/portfolio/static/css/0.7fce162d.chunk.css"
+  },
+  {
+    "revision": "deb58510dd610b5a55cb",
+    "url": "/portfolio/static/css/10.fd0e6272.chunk.css"
+  },
+  {
+    "revision": "e22dc3b60eeac043937b",
+    "url": "/portfolio/static/css/12.2097ecc4.chunk.css"
+  },
+  {
+    "revision": "16891450942ba90c05ce",
+    "url": "/portfolio/static/css/13.ec133b12.chunk.css"
+  },
+  {
+    "revision": "e6bf910fe29f979d8eab",
+    "url": "/portfolio/static/css/4.44298079.chunk.css"
+  },
+  {
+    "revision": "616363c7ce4447364b87",
+    "url": "/portfolio/static/css/5.32dc23c8.chunk.css"
+  },
+  {
+    "revision": "3a95c2fe2e5b850ef954",
+    "url": "/portfolio/static/css/6.e60fd22d.chunk.css"
+  },
+  {
+    "revision": "5f93fdf0691484ee92d2",
+    "url": "/portfolio/static/css/7.7a785e66.chunk.css"
+  },
+  {
+    "revision": "bf503d1d6e6350f9fca4",
+    "url": "/portfolio/static/css/8.0980cb82.chunk.css"
+  },
+  {
+    "revision": "e86eb3b10da5de494099",
+    "url": "/portfolio/static/css/9.39ae4632.chunk.css"
+  },
+  {
+    "revision": "23f5c2323911f2b8e981",
+    "url": "/portfolio/static/css/main.02b72143.chunk.css"
+  },
+  {
+    "revision": "90991ed07a6a8ed1b889",
+    "url": "/portfolio/static/js/0.f217d9ba.chunk.js"
+  },
+  {
+    "revision": "deb58510dd610b5a55cb",
+    "url": "/portfolio/static/js/10.e661355b.chunk.js"
+  },
+  {
+    "revision": "3f018a0b4e9f102c1c83",
+    "url": "/portfolio/static/js/11.b2124afa.chunk.js"
+  },
+  {
+    "revision": "e22dc3b60eeac043937b",
+    "url": "/portfolio/static/js/12.5d6e8577.chunk.js"
+  },
+  {
+    "revision": "16891450942ba90c05ce",
+    "url": "/portfolio/static/js/13.03f3c21b.chunk.js"
+  },
+  {
+    "revision": "91b68480c0c136dfa3c7",
+    "url": "/portfolio/static/js/3.804f2b56.chunk.js"
+  },
+  {
+    "revision": "e6bf910fe29f979d8eab",
+    "url": "/portfolio/static/js/4.60c7d971.chunk.js"
+  },
+  {
+    "revision": "616363c7ce4447364b87",
+    "url": "/portfolio/static/js/5.0d0502d7.chunk.js"
+  },
+  {
+    "revision": "3a95c2fe2e5b850ef954",
+    "url": "/portfolio/static/js/6.c8c730b0.chunk.js"
+  },
+  {
+    "revision": "5f93fdf0691484ee92d2",
+    "url": "/portfolio/static/js/7.181df8da.chunk.js"
+  },
+  {
+    "revision": "bf503d1d6e6350f9fca4",
+    "url": "/portfolio/static/js/8.ba84b1f7.chunk.js"
+  },
+  {
+    "revision": "e86eb3b10da5de494099",
+    "url": "/portfolio/static/js/9.53daf1f5.chunk.js"
+  },
+  {
+    "revision": "23f5c2323911f2b8e981",
+    "url": "/portfolio/static/js/main.4cb2b297.chunk.js"
+  },
+  {
+    "revision": "b0fe0236c2dd8606656e",
+    "url": "/portfolio/static/js/runtime-main.436f5d4a.js"
+  },
+  {
+    "revision": "ffcebcdfa4ef815b50932f8651f56236",
+    "url": "/portfolio/static/media/about.ffcebcdf.webp"
+  },
+  {
+    "revision": "6be3f7ce6a0222c849a92cae01040678",
+    "url": "/portfolio/static/media/bg.6be3f7ce.jpg"
+  },
+  {
+    "revision": "3e9b2c4c763e207dc3af0d7df50561e6",
+    "url": "/portfolio/static/media/codingLand.3e9b2c4c.jpg"
+  },
+  {
+    "revision": "0cade37b05e4911f4e11dcb2f1cc46a6",
+    "url": "/portfolio/static/media/earthHack.0cade37b.jpg"
+  },
+  {
+    "revision": "a9f7ad3dc6d998df5c984f5363913c7f",
+    "url": "/portfolio/static/media/github.a9f7ad3d.svg"
+  },
+  {
+    "revision": "dd4d2432b5e9063204b288360c7b7f25",
+    "url": "/portfolio/static/media/mcistic.dd4d2432.jpg"
+  },
+  {
+    "revision": "4c4bcd628aad8358e0c2c0b5ef222e43",
+    "url": "/portfolio/static/media/meduim.4c4bcd62.png"
+  },
+  {
+    "revision": "d7c390425058519b28813bcfbfd3c693",
+    "url": "/portfolio/static/media/mz2.d7c39042.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e7850259141b145925e644ae94fe659.js"
+  "/portfolio/precache-manifest.ee145f844ba810b7fae06cf9ffe3e641.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
